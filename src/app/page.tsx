d05abaf54a711403b08e90aa2b88848a4712dac3
lumen-limitless <lumenlimitless@gmail.com>
2025-09-21 15:16:29 -0400

    comment out page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { HackathonCard } from "@/components/hackathon-card";
-import BlurFade from "@/components/magicui/blur-fade";
-import BlurFadeText from "@/components/magicui/blur-fade-text";
-import { ProjectCard } from "@/components/project-card";
-import { ResumeCard } from "@/components/resume-card";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Badge } from "@/components/ui/badge";
-import { DATA } from "@/data/resume";
-import Link from "next/link";
-import Markdown from "react-markdown";
+import { HackathonCard } from '@/components/hackathon-card';
+import BlurFade from '@/components/magicui/blur-fade';
+import BlurFadeText from '@/components/magicui/blur-fade-text';
+import { ProjectCard } from '@/components/project-card';
+import { ResumeCard } from '@/components/resume-card';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
+import { DATA } from '@/data/resume';
+import Link from 'next/link';
+import Markdown from 'react-markdown';
 
 const BLUR_FADE_DELAY = 0.04;
 
@@ -22,7 +22,7 @@ export default function Page() {
                 delay={BLUR_FADE_DELAY}
                 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
                 yOffset={8}
-                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
+                text={`Hi, I'm ${DATA.name.split(' ')[0]} 👋`}
               />
               <BlurFadeText
                 className="max-w-[600px] md:text-xl"
@@ -67,14 +67,14 @@ export default function Page() {
                 subtitle={work.title}
                 href={work.href}
                 badges={work.badges}
-                period={`${work.start} - ${work.end ?? "Present"}`}
+                period={`${work.start} - ${work.end ?? 'Present'}`}
                 description={work.description}
               />
             </BlurFade>
           ))}
         </div>
       </section>
-      <section id="education">
+      {/* <section id="education">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 7}>
             <h2 className="text-xl font-bold">Education</h2>
@@ -96,7 +96,7 @@ export default function Page() {
             </BlurFade>
           ))}
         </div>
-      </section>
+      </section> */}
       <section id="skills">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 9}>
@@ -111,7 +111,7 @@ export default function Page() {
           </div>
         </div>
       </section>
-      <section id="projects">
+      {/* <section id="projects">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 11}>
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
@@ -151,8 +151,8 @@ export default function Page() {
             ))}
           </div>
         </div>
-      </section>
-      <section id="hackathons">
+      </section> */}
+      {/* <section id="hackathons">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 13}>
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
@@ -194,7 +194,7 @@ export default function Page() {
             </ul>
           </BlurFade>
         </div>
-      </section>
+      </section> */}
       <section id="contact">
         <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 16}>
@@ -206,13 +206,13 @@ export default function Page() {
                 Get in Touch
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Want to chat? Just shoot me a dm{" "}
+                Want to chat? Just shoot me an email{' '}
                 <Link
-                  href={DATA.contact.social.X.url}
+                  href={DATA.contact.email}
                   className="text-blue-500 hover:underline"
                 >
-                  with a direct question on twitter
-                </Link>{" "}
+                  me@zachlippa.dev
+                </Link>{' '}
                 and I&apos;ll respond whenever I can. I will ignore all
                 soliciting.
               </p>
--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -7,9 +7,9 @@ export const DATA = {
   url: 'https://zachlippa.dev',
   location: 'Rochester, NY',
   locationLink: 'https://www.google.com/maps/place/rochesterny',
-  description: 'Software Engineer, Entrepreneur, and Open Source Enthusiast.',
+  description: 'Software Engineer, Entrepreneur, and Web3 Enthusiast',
   summary:
-    'At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).',
+    'Full-Stack Developer specializing in JavaScript, TypeScript, Rust, and Solidity, with expertise in blockchain technologies and modern web development. Passionate about helping startups and small businesses streamline operations, boost engagement, and expand their digital presence through innovative solutions. Committed to staying at the forefront of new programming paradigms and tools to deliver transformative digital experiences. Enjoys an active lifestyle including hiking, gym workouts, traveling, and spending quality time with family.',
   avatarUrl: '/me.webp',
   skills: [
     'React',
@@ -34,34 +34,34 @@ export const DATA = {
     social: {
       GitHub: {
         name: 'GitHub',
-        url: 'https://dub.sh/dillion-github',
+        url: 'https://dub.sh/NOm6RSe',
         icon: Icons.github,
 
         navbar: true,
       },
       LinkedIn: {
         name: 'LinkedIn',
-        url: 'https://dub.sh/dillion-linkedin',
+        url: 'https://dub.sh/tt6cAmu',
         icon: Icons.linkedin,
 
         navbar: true,
       },
       X: {
         name: 'X',
-        url: 'https://dub.sh/dillion-twitter',
+        url: 'https://dub.sh/7v0qixS',
         icon: Icons.x,
 
         navbar: true,
       },
-      Youtube: {
-        name: 'Youtube',
-        url: 'https://dub.sh/dillion-youtube',
-        icon: Icons.youtube,
-        navbar: true,
-      },
+      // Youtube: {
+      //   name: 'Youtube',
+      //   url: 'https://dub.sh/dillion-youtube',
+      //   icon: Icons.youtube,
+      //   navbar: true,
+      // },
       email: {
         name: 'Send Email',
-        url: '#',
+        url: 'mailto:me@zachlippa.dev',
         icon: Icons.email,
 
         navbar: false,
@@ -71,83 +71,23 @@ export const DATA = {
 
   work: [
     {
-      company: 'Atomic Finance',
-      href: 'https://atomic.finance',
+      company: 'Izzo Golf, Inc.',
+      href: 'https://izzo.com/',
       badges: [],
-      location: 'Remote',
-      title: 'Bitcoin Protocol Engineer',
-      logoUrl: '/atomic.png',
-      start: 'May 2021',
-      end: 'Oct 2022',
-      description:
-        'Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.',
-    },
-    {
-      company: 'Shopify',
-      badges: [],
-      href: 'https://shopify.com',
-      location: 'Remote',
-      title: 'Software Engineer',
-      logoUrl: '/shopify.svg',
-      start: 'January 2021',
-      end: 'April 2021',
-      description:
-        'Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.',
-    },
-    {
-      company: 'Nvidia',
-      href: 'https://nvidia.com/',
-      badges: [],
-      location: 'Santa Clara, CA',
-      title: 'Software Engineer',
-      logoUrl: '/nvidia.png',
-      start: 'January 2020',
-      end: 'April 2020',
-      description:
-        'Architected and wrote the entire MVP of the GeForce Now Cloud Gaming internal admin and A/B testing dashboard using React, Redux, TypeScript, and Python.',
-    },
-    {
-      company: 'Splunk',
-      href: 'https://splunk.com',
-      badges: [],
-      location: 'San Jose, CA',
-      title: 'Software Engineer',
-      logoUrl: '/splunk.svg',
-      start: 'January 2019',
-      end: 'April 2019',
-      description:
-        'Co-developed a prototype iOS app with another intern in Swift for the new Splunk Phantom security orchestration product (later publicly demoed and launched at .conf annual conference in Las Vegas). Implemented a realtime service for the iOS app in Django (Python) and C++; serialized data using protobufs transmitted over gRPC resulting in an approximate 500% increase in data throughput.',
-    },
-    {
-      company: 'Lime',
-      href: 'https://li.me/',
-      badges: [],
-      location: 'San Francisco, CA',
-      title: 'Software Engineer',
-      logoUrl: '/lime.svg',
-      start: 'January 2018',
-      end: 'April 2018',
-      description:
-        'Proposed and implemented an internal ruby API for sending/receiving commands to scooters over LTE networks. Developed a fully automated bike firmware update system to handle asynchronous firmware updates of over 100,000+ scooters worldwide, and provide progress reports in real-time using React, Ruby on Rails, PostgreSQL and AWS EC2 saving hundreds of developer hours.',
-    },
-    {
-      company: 'Mitre Media',
-      href: 'https://mitremedia.com/',
-      badges: [],
-      location: 'Toronto, ON',
+      location: 'Macedon, NY',
       title: 'Software Engineer',
       logoUrl: '/mitremedia.png',
-      start: 'May 2017',
-      end: 'August 2017',
+      start: 'September 2024',
+      end: 'Present',
       description:
         'Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener',
     },
   ],
   education: [
     {
-      school: 'Buildspace',
-      href: 'https://buildspace.so',
-      degree: 's3, s4, sf1, s5',
+      school: 'FreeCodeCamp',
+      href: 'https://www.freecodecamp.org/certification/zachlippa/javascript-algorithms-and-data-structures',
+      degree: 'JavaScript Algorithms and Data Structures Certification',
       logoUrl: '/buildspace.jpg',
       start: '2023',
       end: '2024',
